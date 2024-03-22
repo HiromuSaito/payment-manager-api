ff37db8931b5ea1c86fd5988184ea69acceba82d
HiromuSaito <hiromu.saito@bigtreetc.com>
2024-03-22 22:34:50 +0900

    add: db connect config

---
+++ src/main/kotlin/com/stock/manager/stockmanagerapi/infrastructure/database/DBConfig.kt
@@ -0,0 +1,13 @@
+package com.stock.manager.stockmanagerapi.infrastructure.database
+
+
+import org.jetbrains.exposed.sql.Database
+import org.springframework.context.annotation.Configuration
+import javax.sql.DataSource
+
+@Configuration
+class DBConfig(dataSource: DataSource) {
+    init {
+        Database.connect(dataSource)
+    }
+}
