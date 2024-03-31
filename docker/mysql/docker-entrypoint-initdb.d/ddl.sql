b2cbadf7fcb9a3e83339d1d225613028313fb8ef
HiromuSaito <hiromu.saito@bigtreetc.com>
2024-03-31 16:44:17 +0900

    fix:itemsテーブルの定義を追加

--- docker/mysql/docker-entrypoint-initdb.d/ddl.sql
+++ docker/mysql/docker-entrypoint-initdb.d/ddl.sql
@@ -1,3 +1,11 @@
+CREATE TABLE items (
+    item_code varchar(128) NOT NULL,
+    manufacturer_code date NOT NULL,
+    item_name varchar(200) NOT NULL,
+    is_eop BOOLEAN NOT NULL,
+    PRIMARY KEY (item_code)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE payments (
     id bigint NOT NULL AUTO_INCREMENT,
     payment_type enum('PLANED_PAYMENT', 'NON_PLANED_PAYMENT'),
@@ -5,5 +13,7 @@ CREATE TABLE payments (
     payment_date date NOT NULL,
     payment_amount bigint NOT NULL,
     note varchar(400),
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (item_code)
+        REFERENCES items(item_code)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
